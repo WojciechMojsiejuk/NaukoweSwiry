56cca86fc2a6ee42cc426f4270115ed1a0c3a97a
GramBezEndu <33137304+GramBezEndu@users.noreply.github.com>
2017-10-28 00:16:36 +0200

    Update logika.c

--- logika.c
+++ logika.c
@@ -5,10 +5,9 @@
 int main()
 {
     //Uzywam zmiennej przechowujacej liczby z zakresu od 0 do +18,446,744,073,709,551,615
-    //utevo lux
     unsigned long long input_options = pow(2,N);
     unsigned long long output_options = pow(2,12);
-    printf("%lu %lu ",input_options, output_options);
+    printf("%llu %llu ",input_options, output_options);
 
 
 
