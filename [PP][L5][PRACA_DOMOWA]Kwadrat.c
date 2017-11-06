78fc32fd9ff40ddc2939cca7c611c5c14f61cb52
GramBezEndu <33137304+GramBezEndu@users.noreply.github.com>
2017-11-06 01:19:11 +0100

    Add files via upload

---
+++ [PP][L5][PRACA_DOMOWA]Kwadrat.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define W 3
+#define K 5
+
+void uzupelnij(int tablica[][K])
+{
+    int i, j;
+    for (i=0;i<W;i++)
+    {
+        for (j=0;j<K;j++)
+        {
+            tablica[i][j]=(rand()%11)-5;
+        }
+    }
+}
+
+void wyswietl(int tab[][K])
+{
+    int i,j;
+    for(i=0;i<W;i++)
+    {
+        for(j=0;j<K;j++)
+        {
+            printf("%d\t",tab[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void wspolrzedne(int tab[][K], int k)
+{
+    printf("x");
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+    int tab[W][K], k;
+    uzupelnij(tab);
+    wyswietl(tab);
+    printf("Podaj k: ");
+    scanf("%d", &k);
+    return 0;
+}
