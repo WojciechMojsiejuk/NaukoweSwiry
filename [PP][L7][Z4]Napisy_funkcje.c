8e19a19512f2887a589ba875da9143995f262e4f
GramBezEndu <33137304+GramBezEndu@users.noreply.github.com>
2017-11-11 21:25:40 +0100

    Add files via upload

---
+++ [PP][L7][Z4]Napisy_funkcje.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+
+char* Napis_malymi(const char* napis)
+{
+    char napis2[100];
+    int dl=strlen(napis), i;
+    for(i=0;i<dl;i++)
+    {
+        napis2[i]=napis[i]+32;
+    }
+    napis2[i]='\0';
+    return napis2;
+}
+
+char* Napis_duzymi(const char* napis)
+{
+    char napis2[100];
+    int dl=strlen(napis), i;
+    for(i=0;i<dl;i++)
+    {
+        napis2[i]=napis[i]-32;
+    }
+    napis2[i]='\0';
+    return napis2;
+}
+
+char* Zmiana_wielkosci(const char* napis)
+{
+    return "Nie skonczone";
+}
+
+char* Napis_wspak(const char* napis)
+{
+    return "Nie skonczone";
+}
+
+char* Ten_sam_napis(const char* napis)
+{
+    int dl=strlen(napis), i;
+    char napis2[100];
+    for(i=0;i<dl;i++)
+    {
+        napis2[i]=napis[i];
+    }
+    napis2[i]='\0';
+    return napis2;
+}
+
+int main()
+{
+    char napis[100]="ABCE";
+    printf("Nasz napis to: %s\n", napis);
+    char *napis2=Napis_malymi(napis);
+    printf("Napis malymi: %s\n", napis2);
+    char *napis3=Napis_duzymi(napis);
+    printf("Napis duzymi: %s\n", napis3);
+    char *napis4=Zmiana_wielkosci(napis);
+    printf("Zmiana wielkosci liter: %s\n", napis4);
+    char *napis5=Napis_wspak(napis);
+    printf("Napis wspak: %s\n", napis5);
+    char *napis6=Ten_sam_napis(napis);
+    printf("Ten sam napis: %s\n", napis6);
+}
