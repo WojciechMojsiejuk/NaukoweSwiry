75e8aa423d90d91054da164fe00fdbf135ecc724
yourachan <33199839+yourachan@users.noreply.github.com>
2018-01-07 01:30:06 +0100

    Update main.c

--- projekt/main.c
+++ projekt/main.c
@@ -19,38 +19,11 @@ void test(ADRES lista)
         lista = lista->nast;
     }
 }
-/*void wypisz(ADRES poczatek,const char *nazwa)
-{
-    int i;
-    FILE *plik=fopen(nazwa,"w");
-    if(plik==NULL)
-    {
-      printf("blad otwarcia pliku");
-      return;
-    }
-    while(poczatek!=NULL)
-    {
-        fprintf(plik,"%s:",poczatek->pl);
-        for(i=0;i<(poczatek->ilosc);i++)
-        {
-            fprintf(plik,"%s,",poczatek->ang[i]);
-        }
-        poczatek=poczatek->nast;
-        fprintf(plik,"\n");
-    }
-    fclose(plik);
-}*/
 int main()
 {
     char nazwa[50];
-    char nazwa1[50];
-    printf("podaj nazwe pliku z rozszerzeniem .txt\n");
-    gets(nazwa);
-    ADRES pierwszy=odczyt(nazwa);
    // test(pierwszy);
     menu();
-    //printf("podaj nazwe pliku do ktorego chcesz zapisac wynik z rozszerzeniem .txt\n");
-    //gets(nazwa1);
-    //wypisz(pierwszy,nazwa1);
+    //ADRES pierwszy=odczyt(nazwa);
     return 0;
 }
