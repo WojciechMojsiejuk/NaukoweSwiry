f2bedd6271fc8dbd492124a6ed502e09e894ef16
yourachan <33199839+yourachan@users.noreply.github.com>
2018-01-07 23:04:35 +0100

    Update projekt.h

--- projekt/projekt.h
+++ projekt/projekt.h
@@ -2,7 +2,7 @@
 #define PROJEKT_H_INCLUDED
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include<string.h>
 #define T 10
 #define X 50
 struct slowopl
@@ -18,5 +18,7 @@ typedef struct slowopl SlowoPL;
 void menu();
 ADRES odczyt();
 void wypisz();
+void maxznaczen();
+void kasuj_element();
 
 #endif // PROJEKT_H_INCLUDED
